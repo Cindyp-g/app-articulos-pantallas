4c8dd61b7897006a3850b7b4bcbb7eca453ad824
cindy <cindypeg1992@gmail.com>
2024-06-26 20:16:51 -0600

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { CargarArchivosComponent } from './componentes/cargar-archivos/cargar-archivos.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+const routes: Routes = [
+{path:'login',component:LoginComponent},
+{path:'registro',component:RegistroComponent},
+{path:'cargar-archivo',component:CargarArchivosComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
